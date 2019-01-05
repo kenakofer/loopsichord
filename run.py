d02e021a68333c52adff38cc869bf217deebfc5c
Kenan Bitikofer <kbitikofer@gmail.com>
2019-01-05 12:39:08 -0500

    Fix pygame mixer init hogging cpu

--- run.py
+++ run.py
@@ -10,6 +10,10 @@ def main():
     # initialize game engine
     pygame.init()
     
+    # We only use pygame for drawing, and mixer will hog cpu otherwise 
+    # https://github.com/pygame/pygame/issues/331
+    pygame.mixer.quit()
+    
     # set screen width/height and caption
     screen = pygame.display.set_mode(SCREEN_DIM, pygame.RESIZABLE)
     pygame.display.set_caption('Some digital instrument thing')
